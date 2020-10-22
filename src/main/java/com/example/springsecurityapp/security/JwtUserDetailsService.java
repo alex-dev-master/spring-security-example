9699a6c17e100a9925aefc138c74a621270d3da9
DESKTOP-BGRH97B\Александр <sashakaliberda@gmail.com>
2020-10-22 22:02:39 +0300

    jwt providers and config

---
+++ src/main/java/com/example/springsecurityapp/security/JwtUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.springsecurityapp.security;
+
+import com.example.springsecurityapp.model.User;
+import com.example.springsecurityapp.security.jwt.JwtUser;
+import com.example.springsecurityapp.security.jwt.JwtUserFactory;
+import com.example.springsecurityapp.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User with username:" + username + " not found");
+        }
+
+        JwtUser jwtUser = JwtUserFactory.create(user);
+        log.info("IN loadUserByUsername - user with username: {} successfully loaded", username);
+
+
+        return jwtUser;
+    }
+
+}
